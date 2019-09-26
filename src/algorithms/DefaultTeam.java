326c295efc93d1e0fd0ab5ae656cdd9eef0773c3
Elisabeth <elisabeth.abbaszadeh@gmail.com>
2019-09-26 18:20:32 +0200

    Suppression d'un point de fvs si la solution reste encore valide.

--- src/algorithms/DefaultTeam.java
+++ src/algorithms/DefaultTeam.java
@@ -8,13 +8,13 @@ import java.util.*;
 //< 82,4 +1 point  1,5 point possible si inférieur à je sais pas combien. 2 points de bonus. touche g pour rendre le travail. ressucite 3 et je tue 2 à la place.
 public class DefaultTeam {
 
-  public boolean estArete(Point a, Point b, int edgeThreshold){
-    return a.distance(b)<edgeThreshold;
+  public boolean estArete(Point a, Point b, int edgeThreshold) {
+    return a.distance(b) < edgeThreshold;
   }
 
-  public int degre(Point p, ArrayList<Point> points, int edgeThreshold){
-    int degree=-1;
-    for (Point q: points)
+  public int degre(Point p, ArrayList<Point> points, int edgeThreshold) {
+    int degree = -1;
+    for (Point q : points)
       if (estArete(p, q, edgeThreshold))
         degree++;
     return degree;
@@ -29,32 +29,32 @@ public class DefaultTeam {
 
     int cpt;
 
-    ArrayList<Point> points = (ArrayList<Point>)pointsIn.clone();
-    ArrayList<Point> result = (ArrayList<Point>)pointsIn.clone();
+    ArrayList<Point> points = (ArrayList<Point>) pointsIn.clone();
+    ArrayList<Point> result = (ArrayList<Point>) pointsIn.clone();
     ArrayList<Point> rest;
 
-    for (int i=0;i<100;i++) {
+    for (int i = 0; i < 100; i++) {
       Collections.shuffle(points, new Random(System.nanoTime()));
-      rest = (ArrayList<Point>)points.clone();
+      rest = (ArrayList<Point>) points.clone();
       fvs = new ArrayList<Point>();
 
       while (!e.isValid(points, fvs, edgeThreshold)) {
-        Point choosenOne=rest.get(0);
-        for (Point p: rest)
-          if (degre(p,rest,edgeThreshold)>degre(choosenOne,rest,edgeThreshold))
-            choosenOne=p;
+        Point choosenOne = rest.get(0);
+        for (Point p : rest)
+          if (degre(p, rest, edgeThreshold) > degre(choosenOne, rest, edgeThreshold))
+            choosenOne = p;
 
         fvs.add(choosenOne);
         rest.remove(choosenOne);
       }
-      System.out.println("GR. Current sol: " + result.size() + ". Found next sol: "+fvs.size());
+      System.out.println("GR. Current sol: " + result.size() + ". Found next sol: " + fvs.size());
 
-      if (fvs.size()<result.size())
+      if (fvs.size() < result.size())
         result = fvs;
     }
 
-    fvs=result;
-    System.out.println("Taille fvs : "+fvs.size());
+    fvs = result;
+    System.out.println("Taille fvs : " + fvs.size());
 
     /* Local searching naïf */
     ArrayList<Point> reste = new ArrayList<Point>();
@@ -65,22 +65,22 @@ public class DefaultTeam {
     int i, j, k;
 
     /* Local searching naïf trois pour deux. */
-    continuer=true;
+    continuer = true;
     int r, s;
     Point rr, ss;
-    while(continuer) {
+    while (continuer) {
       // shuffle ici
       Collections.shuffle(fvs);
-      continuer=false;
-      for(i=0; i< fvs.size() && !continuer; i++) {
+      continuer = false;
+      for (i = 0; i < fvs.size() && !continuer; i++) {
         Collections.shuffle(fvs);
-        a=fvs.remove(i);
+        a = fvs.remove(i);
         for (j = i + 1; j < fvs.size() && !continuer; j++) {
           b = fvs.remove(j);
 
           for (k = j + 1; k < fvs.size() && !continuer; k++) {
             c = fvs.remove(k);
-            if((estArete(a, b, edgeThreshold) && (estArete(b, c, edgeThreshold) || estArete(a, c, edgeThreshold)))
+            if ((estArete(a, b, edgeThreshold) && (estArete(b, c, edgeThreshold) || estArete(a, c, edgeThreshold)))
                     || (estArete(b, c, edgeThreshold)) && estArete(a, c, edgeThreshold)) {
               Collections.shuffle(reste);
               for (r = 0; r < reste.size(); r++) {
@@ -104,26 +104,26 @@ public class DefaultTeam {
                   fvs.remove(rr);
               }
             }
-            if(!continuer)
+            if (!continuer)
               fvs.add(c);
           }
-          if(!continuer)
+          if (!continuer)
             fvs.add(b);
         }
-        if(!continuer)
+        if (!continuer)
           fvs.add(a);
       }
     }
 
-    System.out.println("Local searching 3->2 : "+fvs.size());
+    System.out.println("Local searching 3->2 : " + fvs.size());
 
-    result = (ArrayList<Point>)fvs.clone();
-    rest = (ArrayList<Point>)reste.clone();
-    ArrayList<Point> fvsClone= (ArrayList<Point>)fvs.clone();
+    result = (ArrayList<Point>) fvs.clone();
+    rest = (ArrayList<Point>) reste.clone();
+    ArrayList<Point> fvsClone = (ArrayList<Point>) fvs.clone();
 
-    for(int t=0; t<20; t++) {
-      reste = (ArrayList<Point>)rest.clone();
-      fvs = (ArrayList<Point>)fvsClone.clone();
+    for (int t = 0; t < 20; t++) {
+      reste = (ArrayList<Point>) rest.clone();
+      fvs = (ArrayList<Point>) fvsClone.clone();
 
       continuer = true;
       while (continuer) {
@@ -154,17 +154,49 @@ public class DefaultTeam {
         }
       }
 
-      System.out.println("GR. Current sol: " + result.size() + ". Found next sol: "+fvs.size());
+      System.out.println("GR. Current sol: " + result.size() + ". Found next sol: " + fvs.size());
 
-      if (fvs.size()<result.size())
+      if (fvs.size() < result.size())
         result = fvs;
     }
 
-    System.out.println("Local searching 1->2 : "+fvs.size());
+    System.out.println("Local searching 1->2 : " + fvs.size());
 
-    fvs=result;
+    fvs = result;
 
     // rajouter la suppression sans rajout. Vérifier que ça sert à quelque chose
+
+    result = (ArrayList<Point>) fvs.clone();
+    rest = (ArrayList<Point>) reste.clone();
+    fvsClone = (ArrayList<Point>) fvs.clone();
+
+    for (int t = 0; t < 100; t++) {
+      reste = (ArrayList<Point>) rest.clone();
+      fvs = (ArrayList<Point>) fvsClone.clone();
+
+      continuer = true;
+      while (continuer) {
+        // shuffle ici
+        Collections.shuffle(fvs);
+        continuer = false;
+        for (i = 0; i < fvs.size() && !continuer; i++) {
+          a = fvs.remove(i);
+          if (e.isValid(points, fvs, edgeThreshold)) {
+            continuer = true;
+            break;
+          }
+          if (!continuer)
+            fvs.add(a);
+        }
+      }
+
+      System.out.println("GR. Current sol: " + result.size() + ". Found next sol: " + fvs.size());
+
+      if (fvs.size() < result.size())
+        result = fvs;
+    }
+
+    fvs=result;
 
     return fvs;
   }
