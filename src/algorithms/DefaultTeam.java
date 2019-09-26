8b37036463a6d6a9c51b634c5906bd86f64a932a
Elisabeth <elisabeth.abbaszadeh@gmail.com>
2019-09-26 15:52:15 +0200

    Calcul des degrés à la volée.

--- src/algorithms/DefaultTeam.java
+++ src/algorithms/DefaultTeam.java
@@ -12,51 +12,48 @@ public class DefaultTeam {
     return a.distance(b)<edgeThreshold;
   }
 
-  public ArrayList<Point> calculFVS(ArrayList<Point> points, int edgeThreshold) {
-    ArrayList<Point> fvs = new ArrayList<Point>();
+  public int degre(Point p, ArrayList<Point> points, int edgeThreshold){
+    int degree=-1;
+    for (Point q: points)
+      if (estArete(p, q, edgeThreshold))
+        degree++;
+    return degree;
+  }
 
+  public ArrayList<Point> calculFVS(ArrayList<Point> pointsIn, int edgeThreshold) {
     ArrayList<Integer> degres = new ArrayList<>();
     TreeMap<Integer, ArrayList<Point>> degresTries = new TreeMap<>();
-
-    int cpt;
-
-    for(int i=0; i<points.size(); i++) {
-      Point p = points.get(i);
-      cpt=0;
-      for (int j=i+1; j<points.size(); j++) {
-        Point q = points.get(j);
-        if (p.distance(q) < edgeThreshold) {
-          cpt++;
-        }
-      }
-      degres.add(points.indexOf(p), cpt);
-    }
-
-    int degre=-1;
-    ArrayList<Point> al;
-    for(int i=0; i<points.size(); i++) {
-      degre=degres.get(i);
-      if(degresTries.containsKey(degre))
-        degresTries.get(degre).add(points.get(i));
-      else {
-        al=new ArrayList<Point>();
-        al.add(points.get(i));
-        degresTries.put(degre, al);
-      }
-    }
+    ArrayList<Point> fvs = new ArrayList<Point>();
 
     Evaluation e = new Evaluation();
 
-    Point p = null;
-    while(!e.isValid(points, fvs, edgeThreshold)) {
-      al = degresTries.get(degresTries.lastKey());
-      while(al.isEmpty()) {
-        degresTries.remove(degresTries.lastKey());
-        al = degresTries.get(degresTries.lastKey());
+    int cpt;
+
+    ArrayList<Point> points = (ArrayList<Point>)pointsIn.clone();
+    ArrayList<Point> result = (ArrayList<Point>)pointsIn.clone();
+
+    for (int i=0;i<100;i++) {
+      Collections.shuffle(points, new Random(System.nanoTime()));
+      ArrayList<Point> rest = (ArrayList<Point>)points.clone();
+      fvs = new ArrayList<Point>();
+
+      while (!e.isValid(points, fvs, edgeThreshold)) {
+        Point choosenOne=rest.get(0);
+        for (Point p: rest)
+          if (degre(p,rest,edgeThreshold)>degre(choosenOne,rest,edgeThreshold))
+            choosenOne=p;
+
+        fvs.add(choosenOne);
+        rest.remove(choosenOne);
       }
-      if(Math.random()<0.70)
-        fvs.add(p=al.remove(al.size()-1));
+      System.out.println("GR. Current sol: " + result.size() + ". Found next sol: "+fvs.size());
+
+      if (fvs.size()<result.size())
+        result = fvs;
     }
+
+    fvs=result;
+    System.out.println("Taille fvs : "+fvs.size());
 
     /* Local searching naïf */
     ArrayList<Point> reste = new ArrayList<Point>();
