ba436963c9bcdaa5f11214d6c4e3e53814e19b39
Elisabeth <elisabeth.abbaszadeh@gmail.com>
2019-09-28 23:36:50 +0200

    Ajout d'une troisième méthode pour calculer les FVS

--- src/algorithms/DefaultTeam.java
+++ src/algorithms/DefaultTeam.java
@@ -1,21 +1,60 @@
 package algorithms;
 
-import java.awt.Point;
-import java.lang.reflect.Array;
-import java.util.*;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Random;
 
 
 //< 82,4 +1 point  1,5 point possible si inférieur à je sais pas combien. 2 points de bonus. touche g pour rendre le travail. ressucite 3 et je tue 2 à la place.
 public class DefaultTeam {
 
-    public boolean estArete(Point a, Point b, int edgeThreshold){
-        return a.distance(b)<edgeThreshold;
+    public ArrayList<Point> improve(ArrayList<Point> points, ArrayList<Point> fvs, int threshold) {
+        ArrayList<Point> retour = (ArrayList<Point>) fvs.clone();
+        Evaluation eval = new Evaluation();
+        for (Point p : fvs) {
+            retour.remove(p);
+            if (!eval.isValid(points, retour, threshold)) retour.add(p);
+        }
+        return retour;
+    }
+
+    public ArrayList<Point> cleanUp(ArrayList<Point> points) {
+        ArrayList<Point> clone = (ArrayList<Point>) points.clone();
+        ArrayList<Point> neigh;
+        boolean stillAVertex = true;
+        while (stillAVertex) { // tant qu'il reste un vertex de degrés 1
+            stillAVertex = false;
+            ArrayList<Point> clone2 = (ArrayList<Point>) clone.clone();
+            for (Point p : clone2) { // pour chaque point on recherche les voisins
+                neigh = voisins(p, clone);
+                if (neigh.size() <= 1) { // si un seul voisin -> degré 1
+                    stillAVertex = true;
+                    clone.remove(p);
+                }
+            }
+        }
+        ArrayList<Point> retour = (ArrayList<Point>) clone.clone();
+        return retour;
+    }
+
+    private ArrayList<Point> voisins(Point p, ArrayList<Point> vertices) {
+        ArrayList<Point> result = new ArrayList<Point>();
+
+        for (Point point : vertices) if (point.distance(p) < 100 && !point.equals(p)) result.add((Point) point.clone());
+
+        return result;
+    }
+
+    public boolean estArete(Point a, Point b, int edgeThreshold) {
+        return a.distance(b) < edgeThreshold;
     }
 
 
-    public int degre(Point p, ArrayList<Point> points, int edgeThreshold){
-        int degree=-1;
-        for (Point q: points)
+    public int degre(Point p, ArrayList<Point> points, int edgeThreshold) {
+        int degree = -1;
+        for (Point q : points)
             if (estArete(p, q, edgeThreshold) && !p.equals(q))
                 degree++;
         return degree;
@@ -35,7 +74,7 @@ public class DefaultTeam {
 
         Point p;
 
-        for (int i = 0; i < 5000; i++) {
+        for (int i = 0; i < 8000; i++) {
             Collections.shuffle(points, new Random(System.nanoTime() + i));
             fvs = (ArrayList<Point>) points.clone();
             rest = new ArrayList<Point>();
@@ -68,16 +107,16 @@ public class DefaultTeam {
 
         points = (ArrayList<Point>) pointsIn.clone();
 
-        for (int i = 0; i < 5000; i++) {
+        for (int i = 0; i < 8000; i++) {
             Collections.shuffle(points, new Random(System.nanoTime() + i));
             rest = (ArrayList<Point>) points.clone();
             fvs = new ArrayList<Point>();
 
             while (!e.isValid(points, fvs, edgeThreshold)) {
                 Point choosenOne = rest.get(0);
-                for (Point p : rest)
-                    if (degre(p, rest, edgeThreshold) > degre(choosenOne, rest, edgeThreshold))
-                        choosenOne = p;
+                for (Point p3 : rest)
+                    if (degre(p3, rest, edgeThreshold) > degre(choosenOne, rest, edgeThreshold))
+                        choosenOne = p3;
 
                 if ((d = degre(choosenOne, rest, edgeThreshold)) > degreMax)
                     degreMax = d;
@@ -91,20 +130,57 @@ public class DefaultTeam {
                 result = fvs;
         }
 
-        fvs = result;
         System.out.println("Taille fvs après méthode 2 : " + fvs.size());
+        int i;
+        ArrayList<Point> fvs_tmp = null;
+        for(int ite = 0 ; ite < 2; ite++) {
+            fvs = (ArrayList<Point>) points.clone();
+            Point maxi;
+            maxi = pointsIn.parallelStream().max(Comparator.comparingInt(element -> voisins(element, pointsIn).size())).get();
+            ArrayList<Point> test = (ArrayList<Point>) points.clone();
+            if(ite == 1){
+                test.remove(maxi);
+                maxi = test.parallelStream().max(Comparator.comparingInt(element -> voisins(element, test).size())).get();
+            }
+            ArrayList<Point> neigh = voisins(maxi, pointsIn);
 
-        /* Local searching naïf */
+             i = 0;
+             int high = neigh.size();
+            while (i <= high) {
+                for (Point p2 : pointsIn) {
+                    if (voisins(p2, pointsIn).size() <= i) {
+                        fvs.remove(p2);
+                        if (!e.isValid(pointsIn, fvs, edgeThreshold)) fvs.add(p2);
+                    }
+                }
+                i++;
+            }
+            int score = fvs.size();
+            int scoreTmp = Integer.MAX_VALUE;
+            while (score < scoreTmp) {
+                scoreTmp = fvs.size();
+                improve(points, fvs, edgeThreshold);
+                cleanUp(fvs);
+                score = fvs.size();
+            }
+            if(ite==0){
+                fvs_tmp = (ArrayList<Point>) fvs.clone();
+            } else {
+                if(fvs.size() > fvs_tmp.size()){
+                    fvs = (ArrayList<Point>) fvs_tmp.clone();
+                }
+            }
+        }
+
+        // Local searching naïf
         ArrayList<Point> reste = new ArrayList<Point>();
         reste.addAll(points);
         reste.removeAll(fvs);
         boolean continuer = true;
         Point a, b, c;
-        int i, j, k;
+        int j, k;
 
-
-        // le faire itérer 10/20 fois :
-        /* Local searching naïf trois pour deux. */
+        // Local searching naïf trois pour deux.
         continuer = true;
         int r=0, s=0;
         Point rr, ss;
